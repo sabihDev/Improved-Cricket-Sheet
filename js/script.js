37d7d2540fa3863782fdee7e52b47b42d09b3e9d
sabihDev <8203sabi.rahman@gmail.com>
2023-09-23 06:03:50 -0700

    balls menu created

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 // import { Hide, Show } from "./Resueables.js";
 
 const liveMenuContainer = document.querySelector(".live-container");
-const scorecaedMenuContainer = document.querySelector(".scorecard-container");
+const scorecardMenuContainer = document.querySelector(".scorecard-container");
 const ballsMenuButton = document.querySelector('.menu__balls');
 
 const scorecardMenuButton = document.querySelector(".menu__scorecard");
 const liveMenuButton = document.querySelector(".menu__live");
+const ballsMenuContainer = document.querySelector(".balls-menu-container");
 
 const teamAButton = document.querySelector(".innings-buttons__first-team");
 const teamBButton = document.querySelector(".innings-buttons__second-team");
+
+const firstInningButton = document.querySelector('.innings-buttons__first');
+const secondInningButton = document.querySelector('.innings-buttons__second');
 
 const hr = document.querySelector(".animate-menu");
 
@@ -20,31 +24,72 @@ scorecardMenuButton.onclick = () => {
         ballsMenuButton.classList.remove("active");
     }
 
-    if (scorecaedMenuContainer.classList.contains("hidden")) {
-        scorecaedMenuContainer.classList.remove('hidden');
+    if (scorecardMenuContainer.classList.contains("hidden")) {
+        scorecardMenuContainer.classList.remove('hidden');
     }
 
-    liveMenuContainer.classList.add('hidden');
+    if (!liveMenuContainer.classList.contains("hidden")) {
+        liveMenuContainer.classList.add('hidden');
+    }
+
+    if (!ballsMenuContainer.classList.contains("hidden")) {
+        ballsMenuContainer.classList.add("hidden")
+    }
+
     scorecardMenuButton.classList.add("active");
     hr.style.transform = 'translateX(0) translateY(7px)';
     hr.style.width = '63px';
 }
 
 liveMenuButton.onclick = () => {
-    if (!scorecardMenuButton.classList.contains('active')) {
+    if (scorecardMenuButton.classList.contains('active')) {
         scorecardMenuButton.classList.remove("active");
     }
-    if (!ballsMenuButton.classList.contains('active')) {
+    if (ballsMenuButton.classList.contains('active')) {
         ballsMenuButton.classList.remove("active");
     }
 
-    if (!liveMenuContainer.classList.contains("hidden")) {
-    liveMenuContainer.classList.add("hidden");
+    if (liveMenuContainer.classList.contains("hidden")) {
+        liveMenuContainer.classList.remove("hidden");
     }
 
-    scorecaedMenuContainer.classList.add("hidden");
+    if (!scorecardMenuContainer.classList.contains("hidden")) {
+        scorecardMenuContainer.classList.add('hidden');
+    }
+
+    if (!ballsMenuContainer.classList.contains("hidden")) {
+        ballsMenuContainer.classList.add("hidden")
+    }
+
+    scorecardMenuContainer.classList.add("hidden");
     liveMenuButton.classList.add('active');
     hr.style.transform = 'translateX(-53px) translateY(7px)';
+    hr.style.width = '30px';
+}
+
+ballsMenuButton.onclick=()=>{
+    if (scorecardMenuButton.classList.contains('active')) {
+        scorecardMenuButton.classList.remove("active");
+    }
+    if (liveMenuButton.classList.contains('active')) {
+        liveMenuButton.classList.remove("active");
+    }
+
+    if (ballsMenuContainer.classList.contains("hidden")) {
+        ballsMenuContainer.classList.remove("hidden");
+    }
+
+    if (!scorecardMenuContainer.classList.contains("hidden")) {
+        scorecardMenuContainer.classList.add('hidden');
+    }
+
+    if (!liveMenuContainer.classList.contains("hidden")) {
+        liveMenuContainer.classList.add("hidden")
+    }
+
+    scorecardMenuContainer.classList.add("hidden");
+    ballsMenuButton.classList.add('active');
+    hr.style.transform = 'translateX(53px) translateY(7px)';
     hr.style.width = '30px';
 }
 
@@ -63,3 +108,20 @@ teamBButton.onclick = () => {
     teamAButton.style.background = 'rgb(237, 237, 237)';
     teamAButton.style.color = 'black';
 }
+
+firstInningButton.onclick=()=>{
+    if (secondInningButton.classList.contains("active-button")) {
+        secondInningButton.classList.remove("active-button");
+    }
+    firstInningButton.classList.add("active-button");
+}
+
+secondInningButton.onclick=()=>{
+    if (firstInningButton.classList.contains("active-button")) {
+        firstInningButton.classList.remove("active-button");
+    }
+    secondInningButton.classList.add("active-button");
+}
+
+
+
